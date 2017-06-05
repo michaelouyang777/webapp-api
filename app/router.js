bf2ae48d8ebcb88e98dd7ad7fc4e3aca9a507338
littleMarkZ <1581102789@163.com>
2017-06-05 10:52:20 +0800

    增加会议的路由配置

--- app/router.js
+++ app/router.js
@@ -3,4 +3,6 @@ module.exports = app =>{
   app.post('/api/v1/user/login', 'user.login');
   app.get('/api/v1/issues/todo/:status', 'issues.todo');
   app.resources('issues', '/api/v1/issues', 'issues');
+  app.get('/api/v1/meeting/todo/:status', 'meeting.todo');
+  app.resources('meeting', '/api/v1/meeting', 'meeting')
 }
